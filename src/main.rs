815461984d1411edd5177141afb7125914895948
ZaharDomolego <domolegozahar@gmail.com>
2023-02-02 15:21:26 +0800

    Сгенерировал энтропию

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-extern  crate crypto;
+use rand::Rng;
 fn main() {
     // crypto  
-    println!("Hello, world!");
+    let random_bytes: Vec<u8> = (0..256)
+        .map(|_| { rand::thread_rng().gen_range(0, 2) })
+        .collect();
+    println!("{:?}", random_bytes);
 
 }
