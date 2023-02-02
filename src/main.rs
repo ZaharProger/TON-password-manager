e6f48d919a34b64f9e7b9e227f3d5720b1eae7e4
ZaharDomolego <domolegozahar@gmail.com>
2023-02-02 15:45:31 +0800

    Переделал генерацию энтропии

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use rand::Rng;
 fn main() {
-    
-    let random_bytes: Vec<u8> = (0..256)
-        .map(|_| { rand::thread_rng().gen_range(0, 2) })
-        .collect();
-    println!("{:?}", random_bytes);
+    let mut random = rand::thread_rng();
+    let mut random_bytes = String::from("");
+    for _ in 0..256 {
+        random_bytes += &random.gen_range(0, 2).to_string();
+    }
+    println!("{}", random_bytes);
 }
