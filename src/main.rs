d0eed1da6dd110b0ed12703322beae2be3913f16
ZaharDomolego <domolegozahar@gmail.com>
2023-02-05 22:54:52 +0800

    Добавил генерацию Seed через PBKDF2 и поменял String на Vec<u8> у энтропии

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::{fs, collections::HashMap};
+use std::{fs::read_to_string, collections::HashMap};
 
 use rand::Rng;
-use crypto::{sha2::Sha256, digest::Digest};
+use crypto::{sha2::Sha256, sha2::Sha512, hmac::Hmac, digest::Digest, pbkdf2::pbkdf2};
 
 fn main() {
     //Инициализируем генератор рандома
     let mut random = rand::thread_rng();
 
     //Читаем слова с вордлиста и формируем словарь
-    let word_string = fs::read_to_string("src\\bibs.txt").unwrap();
+    let word_string = read_to_string("src\\bibs.txt").unwrap();
     let word_list: Vec<&str> = word_string.split("\r\n").collect();
     let mut word_map: HashMap<i32, &str> = HashMap::new();
 
@@ -19,28 +19,25 @@ fn main() {
     }
 
     //Генерируем энтропию
-    let mut entropy = String::new();
-    for _ in 0..256 {
-        entropy.push_str(&random.gen_range(0, 2).to_string());
-    }
-    println!("Исходная энтропия: {}", &entropy);
+    let mut entropy: Vec<u8> = (0..256)
+        .map(|_| random.gen_range(0, 2))
+        .collect();
+
+    println!("Исходная энтропия: {:?}", &entropy);
 
     //Получаем хэш по энтропии
     let mut sha256 = Sha256::new();
-    let mut hash: Vec<u8> = vec![0; 32];
+    let mut buffer: Vec<u8> = vec![0; 32];
 
-    sha256.input_str(&entropy);
-    sha256.result(&mut hash);
-
-    //Создаем бинарную строку из хэша
-    let mut binary_hash = String::new();
-    for byte in &hash {
-        binary_hash.push_str(&format!("{:08b}", byte));
-    }
+    sha256.input(&entropy);
+    sha256.result(&mut buffer);
 
     //Добавляем контрольную сумму к энтропии
-    entropy.push_str(&binary_hash[..8]);
-    println!("Окончательная энтропия: {}", &entropy);
+    let first_byte = format!("{:08b}", buffer[0]);
+    for bit in first_byte.chars() {
+        entropy.push(bit.to_digit(2).unwrap() as u8);
+    }
+    println!("Окончательная энтропия: {:?}", &entropy);
 
     //Формируем мнемоническую фразу
     let mut mnemonic: Vec<&str> = Vec::new();
@@ -48,8 +45,11 @@ fn main() {
     let mut bunch_end = 11;
     
     while bunch_start != entropy.len() {
-        let entropy_slice = &entropy[bunch_start..bunch_end];
-        let word_index = isize::from_str_radix(entropy_slice, 2);
+        let entropy_slice: String = entropy[bunch_start..bunch_end]
+            .iter()
+            .map(|entropy_digit| entropy_digit.to_string())
+            .collect();
+        let word_index = isize::from_str_radix(&entropy_slice, 2);
         mnemonic.push(word_map[&(word_index.unwrap() as i32)]);
 
         bunch_start = bunch_end;
@@ -57,5 +57,31 @@ fn main() {
     }
     
     println!("Мнемоника: {:?}", mnemonic);
+
+    //Генерируем Seed через PBKDF2
+    let iterations = 4096;
+    let salt: Vec<u8> = (0..128)
+        .map(|_| random.gen_range(0, 2))
+        .collect();
+    
+    pbkdf2(&mut Hmac::new(
+        Sha512::new(), 
+        &entropy), 
+        &salt, 
+        iterations, 
+        &mut buffer
+    );
+
+    let mut seed: Vec<u8> = Vec::new();
+    let seed_binary_string: String = buffer
+        .iter()
+        .map(|byte| format!("{:08b}", byte))
+        .collect();
+
+    for bit in seed_binary_string.chars() {
+        seed.push(bit.to_digit(2).unwrap() as u8);
+    }
+
+    println!("Seed: {:?}", seed);
 
 }
